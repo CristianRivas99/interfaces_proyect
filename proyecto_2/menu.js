ed6846d8e826dfde9f708ac8ef87ff330da85195
CristianRivas99 <115761574+CristianRivas99@users.noreply.github.com>
2023-01-31 19:25:04 -0500

    first commit

---
+++ proyecto_2/menu.js
@@ -0,0 +1,14 @@
+//  seleccionamos los dos elementos que serán clickables
+
+const toggleButton = document.getElementById("button-menu");
+const navWrapper = document.getElementById("nav");
+
+/* 
+  cada ves que se haga click en el botón 
+  agrega y quita las clases necesarias 
+  para que el menú se muestre.
+*/
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("close");
+  navWrapper.classList.toggle("show");
+});
